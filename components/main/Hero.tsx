c1a2a1012d0f16ba42431f82149d5b5e8e438a5d
ceosmore <ceosmore@gmail.com>
2024-03-31 18:35:41 -0500

    Añadir estilos y contenido al componente Hero, actualizar dependencias

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeroContent from '../sub/HeroContent';
 
 const Hero = () => {
     return (
@@ -11,8 +12,8 @@ const Hero = () => {
             >
                 <source src='/blackhole.webm' type='video/webm' />
             </video>
+            <HeroContent />
         </div>
     );
 };
-
 export default Hero;
