bc5960cdd81dd9e7f5d9964293bfd1153d8ea02f
ceosmore <ceosmore@gmail.com>
2024-03-31 19:28:56 -0500

    Añadir fondo de inicio y actualizar estilos en HeroContent

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,15 +1,16 @@
 "use client"
 
-import React from 'react'
-import { motion } from 'framer-motion'
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
-import { SparklesIcon } from '@heroicons/react/16/solid'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion';
+import { SparklesIcon } from '@heroicons/react/24/solid';
+import Image from "next/image";
 
 const HeroContent = () => {
     return (
         <motion.div
-            initial="Hidden"
-            animate="Visible"
+            initial="hidden"
+            animate="visible"
             className='flex flex-row items-center justify-center px-20 mt-40  w-full z-[20]'
         >
             <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
@@ -23,14 +24,17 @@ const HeroContent = () => {
                         Fullstack Developer Portfolio
                     </h1>
                 </motion.div>
+
                 <motion.div
                     variants={slideInFromLeft(0.5)}
-
                     className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
                 >
                     <span>
                         Providing
-                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best </span>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>
+                            {" "}
+                            the best {" "}
+                        </span>
                         project experience
                     </span>
 
@@ -55,7 +59,7 @@ const HeroContent = () => {
                 variants={slideInFromRight(0.8)}
                 className='w-full h-full flex justify-center items-center'
             >
-                <img
+                <Image
                     src='/mainIconsdark.svg'
                     alt='work icons'
                     height={650}
